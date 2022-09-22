f1a94d424a8c7a4a8990c49d1318f0aecda091aa
ajaxli <ajaxli@qq.com>
2022-09-22 12:21:00 +0800

    1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chuanlii/test_mod_1
 
 go 1.18
+
+require github.com/chuanlii/test_mod_base v0.1.0
--- m1.go
+++ m1.go
@@ -1,10 +1,14 @@
 package test_mod_1
 
-import "fmt"
+import (
+	"fmt"
+	base "github.com/chuanlii/test_mod_base"
+)
 
 func init() {
 	fmt.Println("testmod1")
 }
 func F1() (string, error) {
+	base.Base()
 	return "f1", nil
 }
